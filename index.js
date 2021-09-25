3b55503ed6f9dbf9cf2dad85b506c2b5ae5756f1
apradyut <adarsh.pradyut@gmail.com>
2021-09-25 21:30:54 +0530

    old status retreival and Patching complete

--- index.js
+++ index.js
@@ -14,41 +14,100 @@ exports.handler = async (event, param2) => {
 
   let response;
   let statusCode;
-  if (event.path !== Constants.APIEntryPath) {
-    console.log(
-      "Miss configuration in API Gateway. Got: ",
-      event.path,
-      " Expected: ",
-      Constants.APIEntryPath
-    );
-    return;
-  }
+
   const requestBody = event.body ? JSON.parse(event.body) : null;
   var queryString = event.queryStringParameters;
   console.log(queryString);
+  console.log("Request Body: ", requestBody);
 
   switch (true) {
-    case event.httpMethod === Constants.GetMethod:
-      response = getHardwareState(
+    case event.httpMethod === Constants.GetMethod &&
+      event.path === Constants.APIEntryPath:
+      response = await getHardwareState(
         event.queryStringParameters.hardwareId,
         event.queryStringParameters.needCurrent
       );
       statusCode = 200;
       break;
-    case event.httpMethod === Constants.PatchMethod:
-      response = "Added";
+    case event.httpMethod === Constants.PatchMethod &&
+      event.path === Constants.APIEntryPath:
+      response = await updateHardwareState(requestBody);
       statusCode = 201;
       break;
   }
-  return buildResponse(statusCode, response);
+  let finalResponse = buildResponse(statusCode, response);
+  console.log(finalResponse);
+  return finalResponse;
 };
 
-let getHardwareState = function (hardwareId, needCurrent = false) {
-  return {
-    LastPingTime: Date.now(),
-    HardwareId: hardwareId,
-    LastIrrigationTime: Date.now(),
+let getHardwareState = async function (hardwareId, needCurrent = false) {
+  const param = {
+    TableName: hardwareStateTableName,
+    Key: {
+      hardwareId: hardwareId,
+    },
   };
+
+  return await dynamodb
+    .get(param)
+    .promise()
+    .then(
+      (response) => {
+        return response.Item;
+      },
+      (error) => {
+        console.error("Error: ", error);
+      }
+    );
+};
+
+let updateHardwareState = async function (hardwareState) {
+  // try {
+  //   let existingHardwareState = await getHardwareState(
+  //     hardwareState.hardwareId,
+  //     true
+  //   );
+
+  //   if (existingHardwareState) {
+  //     await deleteHardwareState(hardwareState.hardwareId);
+  //   }
+  // } catch (error) {
+  //   console.log("Error in updateHardwareState: ", error);
+  // }
+
+  await createHardwareState(hardwareState);
+};
+
+let deleteHardwareState = async function (hardwareId) {
+  let params = {
+    TableName: hardwareStateTableName,
+    Key: {
+      hardwareId: hardwareId,
+    },
+  };
+  return await dynamodb.delete(params).promise();
+};
+
+let createHardwareState = async function (hardwareState) {
+  let params = {
+    TableName: hardwareStateTableName,
+    Item: hardwareState,
+  };
+  return await dynamodb
+    .put(params)
+    .promise()
+    .then(
+      () => {
+        const body = {
+          Operation: "SAVE",
+          Message: "SUCCESS",
+          Item: hardwareState,
+        };
+      },
+      (error) => {
+        console.error("Error: ", error);
+      }
+    );
 };
 
 let buildResponse = function (statusCode, body) {
